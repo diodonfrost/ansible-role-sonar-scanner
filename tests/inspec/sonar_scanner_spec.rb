f860f484ab47ff952c8405547217578b0df3039c
diodonfrost <diodon.frost@diodonfrost.me>
2019-05-29 21:40:43 +0200

    add windows support in inspec test

--- tests/inspec/sonar_scanner_spec.rb
+++ tests/inspec/sonar_scanner_spec.rb
@@ -1,10 +1,13 @@
 # Check if sonar-scanner is functionnal
 
+sonar_scanner_command = 'sonar-scanner --version'
+sonar_scanner_command = 'sonar-scanner.bat --version' if os.family == 'windows'
+
 control 'sonar-scanner-01' do
   impact 1.0
   title 'Sonar-scanner install'
   desc 'Sonar-scanner should be functionnal'
-  describe command('sonar-scanner --version') do
+  describe command(sonar_scanner_command) do
     its('exit_status') { should eq 0 }
   end
 end
